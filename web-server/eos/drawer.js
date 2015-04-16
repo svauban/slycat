28a22327d2087b5ca50f06bb33785d0b5bc2cba1
Sylvain Bernard <srbdevelopment@gmail.com>
2015-04-15 23:33:31 -0600

    Very basic zooming and panning prototype to get us started.

--- web-server/eos/drawer.js
+++ web-server/eos/drawer.js
@@ -1,7 +1,117 @@
 var Drawer = (function() {
   'use strict';
 
-  function Drawer() {}
+  var draw = function(that) {
+    that.data.forEach(function(set, i) {
+      that.svg.select('.x.axis').call(that.xaxis);
+      that.svg.select('.y.axis').call(that.yaxis);
+      that.svg.select('#line_' + i).attr('d', that.line);
+    });
+  };
+
+  var onzoom = function(that) {
+    draw(that);
+  };
+
+
+  function Drawer(selector) {
+    this.selector = selector;
+    this.data = null;
+    this.generator = null;
+    this.xaxis = null;
+    this.yaxis = null;
+    this.line = null;
+  }
+
+  Drawer.prototype.init = function(nlines, npoints) {
+    var that = this;
+
+    that.generator = new Generator(nlines);
+    that.data = that.generator.generate(npoints);
+  };
+
+  Drawer.prototype.draw = function() {
+    var that = this;
+    var margins = { top: 20, right: 20, bottom: 30, left: 50 };
+    var width = parseInt(d3.select(that.selector).style('width'), 10) - margins.left - margins.right;
+    var height = parseInt(d3.select(that.selector).style('height'), 10) - margins.top - margins.bottom;
+
+    var x = d3.scale.linear()
+      .range([0, width])
+      .domain([0, that.data[0].length]);
+
+    var ymax = Number.MIN_VALUE;
+    that.data.forEach(function(set) {
+      var tmp = d3.max(set);
+      ymax = tmp > ymax ? tmp : ymax;
+    });
+
+    var y = d3.scale.linear()
+      .range([height, 0])
+      .domain([1, ymax]);
+
+    that.xaxis = d3.svg.axis()
+      .scale(x)
+      .orient('bottom');
+
+    that.yaxis = d3.svg.axis()
+      .scale(y)
+      .orient('left');
+
+    that.line = d3.svg.line()
+      .x(function(d, i) {
+        return x(i);
+      })
+      .y(function(d, i) {
+        return y(d);
+      });
+
+    var zoom = d3.behavior.zoom()
+      .x(x)
+      .y(y)
+      .on('zoom', function() { onzoom(that); })
+
+    that.svg = d3.select(that.selector).append('svg')
+      .attr('width', width + margins.left + margins.right)
+      .attr('height', height + margins.top + margins.bottom)
+      .append('g')
+        .attr('transform', 'translate(' + margins.left + ', ' + margins.top + ')');
+
+    that.svg.append('clipPath')
+      .attr('id', 'clip')
+    .append('rect')
+      .attr('x', x(0))
+      .attr('y', y(1))
+      .attr('width', x(1) - x(0))
+      .attr('height', y(0) - y(1));
+
+    that.svg.append('g')
+      .classed('x axis', true)
+      .attr('transform', 'translate(0,' + height + ')');
+
+    that.svg.append('g')
+      .classed('y axis', true);
+
+    that.svg.append('path')
+      .attr('class', 'line')
+      .attr('clip-path', 'url(#clip)');
+
+    that.svg.append('rect')
+      .attr('id', 'pane')
+      .attr('width', width)
+      .attr('height', height)
+      .call(zoom);
+
+    that.data.forEach(function(set, i) {
+      that.svg.append('path')
+        .datum(set)
+        .classed('line', true)
+        .attr('id', 'line_' + i);
+    });        
+
+    /** initial render */
+    draw(that);
+  };
 
   return Drawer;
 
--- web-server/eos/generator.js
+++ web-server/eos/generator.js
@@ -1,7 +1,42 @@
 var Generator = (function(){
   'use strict';
 
-  function Generator() {}
+  var Y_THRESHOLD = 1000;
+  var NEXT_STEP = 7;
+
+  var generate = function(npoints) {
+    var arr = [];
+    var seed = Math.random() * Y_THRESHOLD;
+    arr.push(seed);
+
+    for (var i = 0; i < npoints-1; i += 1) {
+      var val = Math.random() > 0.5 ? seed + (NEXT_STEP * Math.random()) : seed - (NEXT_STEP * Math.random());
+
+      if (val < 0) val = 0;
+      if (val > Y_THRESHOLD) val = Y_THRESHOLD;
+
+      arr.push(val);
+      seed = val;
+    }
+
+    return arr;
+  };
+
+
+  function Generator(nlines) {
+    nlines = nlines || 1;
+    this.lines = [];
+    this.nlines = nlines;
+  }
+
+  Generator.prototype.generate = function(npoints) {
+    var that = this;
+
+    for (var i = 0; i < that.nlines; i += 1)
+      that.lines.push(generate(npoints));
+
+    return that.lines;
+  };
 
   return Generator;
 
